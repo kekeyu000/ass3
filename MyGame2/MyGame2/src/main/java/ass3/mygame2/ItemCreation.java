4314d744db935606631f782813d805ae29684d49
kekeyu000 <62225615+kekeyu000@users.noreply.github.com>
2021-05-25 17:04:55 +1000

    add description for item1 & item 2
    item1= new Item("item1","Just a cute toy",0);// add item1 and its description
    item2= new Item("item2","Just a watermelon",0);// add item2 and its description
    
    allItemsInGame.add(item1);//put item1 in the game
    allItemsInGame.add(item2);// put item2 in the game

--- MyGame2/MyGame2/src/main/java/ass3/mygame2/ItemCreation.java
+++ MyGame2/MyGame2/src/main/java/ass3/mygame2/ItemCreation.java
@@ -28,10 +28,14 @@ public class ItemCreation
         excaliburSword = new Item("excaliburSword", "The legendary Excalibur", 100);
         key = new Item("key", "It has a shape of a heart", 100);
         frontGateKey = new Item("frontGateKey", "To open the front gate door", 100);
+        item1= new Item("item1","Just a cute toy",0);// add item1 and its description
+        item2= new Item("item2","Just a watermelon",0);// add item2 and its description
         
         allItemsInGame.add(excaliburSword);
         allItemsInGame.add(key);
         allItemsInGame.add(frontGateKey);
+        allItemsInGame.add(item1);//put item1 in the game
+        allItemsInGame.add(item2);// put item2 in the game
     }
    
     public Item getItem(String stringItem){
